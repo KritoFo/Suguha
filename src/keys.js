e171e009d774f7c9dc58b0efa70723ba5a997759
KritoFo <eimmysuguha@gmail.com>
2024-09-25 20:41:13 -0500

    Rama Secundaria

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         host: 'localhost',
-        port: '3307',
+        port: '3306',
         user: 'root',
-        password: '1234',
+        password: 'Sofia301214*',
         database: 'suguha'
     }
 }
